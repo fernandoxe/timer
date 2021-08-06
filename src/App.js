fba73bff34f0b185e5dbe414e1db6f171b14d0ee
Fernando Parra <fernandoxe@gmail.com>
2021-08-06 00:56:49 -0300

    feat: home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Home from './pages/Home';
+
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Home />
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import Timer from '../../components/Timer';
+
+const Home = () => {
+  return (
+    <Timer />
+  );
+};
+
+export default Home;
